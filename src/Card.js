2a50a360e9971e8929badc7a2be903b1ecf9bc1f
Marcos Henriques <marcostiagofh@gmail.com>
2018-11-30 21:57:17 -0200

    title and prettier badge

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
-import React from "react"
+import React from "react";
 
-export default({ 
-    card,
-    canMoveLeft,
-    canMoveRight,
-    onMoveLeft,
-    onMoveRight
- }) => (
-    <div className="card">
-        {canMoveLeft && <button onClick={onMoveLeft}>{'<'}</button>}
-        <span className="title">{card.name}</span>
-        {canMoveRight && <button onClick={onMoveRight}>{'>'}</button>}
-    </div>
-)
+export default ({
+  card,
+  canMoveLeft,
+  canMoveRight,
+  onMoveLeft,
+  onMoveRight
+}) => (
+  <div className="card">
+    {canMoveLeft && <button onClick={onMoveLeft}>{"<"}</button>}
+    <span className="title">{card.name}</span>
+    {canMoveRight && <button onClick={onMoveRight}>{">"}</button>}
+  </div>
+);
